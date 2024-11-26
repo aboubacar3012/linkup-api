155a92da04c51e7aea9d3f3b86014c021f366351
DIALLO ABOUBACAR <a.diallo3012@gmail.com>
2024-11-26 21:07:26 +0100

    Nettoyage des imports dans users.controller.ts : suppression des imports inutilisés

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { ApiBearerAuth, ApiOkResponse } from '@nestjs/swagger';
